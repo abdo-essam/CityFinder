f034c4a0033e3a84ae785edd8c9de41db1e71374
abdo <abdo.e.mosa@gmail.com>
2024-08-07 03:50:39 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation( "androidx.recyclerview:recyclerview:1.3.2")
+    implementation( "com.google.code.gson:gson:2.10.1")
+    implementation( "org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    implementation( "com.google.android.material:material:1.12.0")
+    implementation( "androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation( "androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
 }
