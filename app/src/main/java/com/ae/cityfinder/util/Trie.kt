dec0bd6386ae6bffaa3081aa745e52b4a8d3e370
abdo <abdo.e.mosa@gmail.com>
2024-08-07 04:02:48 +0300

    Add Trie Data Structure for Efficient Prefix Search

---
+++ app/src/main/java/com/ae/cityfinder/util/Trie.kt
@@ -0,0 +1,18 @@
+package com.ae.cityfinder.util
+
+import com.ae.cityfinder.model.City
+
+class Trie {
+    private class TrieNode {}
+
+    private val root = TrieNode()
+
+    fun insert(word: String, city: City) {}
+    fun search(prefix: String): List<City> {
+        return emptyList()
+    }
+    private fun collectAllWords(node: TrieNode): List<City> {
+        return emptyList()
+    }
+
+}
